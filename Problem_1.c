91cd8fe9c36669f62f543c59543762beebff3f86
DushyanthNayak <81621374+DushyanthNayak@users.noreply.github.com>
2021-09-01 06:22:57 +0000

    incorrect problems

--- Problem_1.c
+++ Problem_1.c
@@ -9,11 +9,17 @@
 
 int main()
 {
-    int N;
+    int i,N;
+    int sum=0;
     printf("Enter the value for N\n");
     scanf("%d", &N);
 
     //Write your code here
+    for(i=1;i<=N;i++)
+    {
+    sum=sum+i;
+    }
+    printf("the answer is %d",sum);
        
        
        
--- Problem_4.c
+++ Problem_4.c
@@ -5,9 +5,18 @@
 
 int main()
 {
-  	int n;
+  	int n,i,j;
+    printf("enter the value of n\n");
 
   	scanf("%d", &n);
+    for(i=1;i<=n;i++)
+    {
+      for(j=1;j<=i;j++)
+      {
+        printf("*");
+      }
+      printf("\n");
+    }
 
   	//Write your code here
   	
